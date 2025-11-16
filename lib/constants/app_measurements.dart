a2c9831d706019f61a883148e701d8f0814eb971
Moaz Osama <wwwmoazosama525@gmail.com>
2025-11-16 23:19:40 +0200

    feat: add class AppMeasurements

---
+++ lib/constants/app_measurements.dart
@@ -0,0 +1,11 @@
+abstract class AppMeasurements {
+  static const double mobileScreenWidth = 800;
+  static const double tabletScreenWidth = 1024;
+  static const double desktopScreenWidth = 1440;
+
+  static const double appBarHeight = 56;
+  static const double buttonHeight = 48;
+  static const double paddingSmall = 8;
+  static const double paddingMedium = 16;
+  static const double paddingLarge = 24;
+}
