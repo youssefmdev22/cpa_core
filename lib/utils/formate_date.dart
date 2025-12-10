987b65c5b9e6b2f4a54471448179aa8518b9051d
Moaz Osama <wwwmoazosama525@gmail.com>
2025-12-10 12:03:40 +0200

    feat: add new formate date

--- lib/utils/formate_date.dart
+++ lib/utils/formate_date.dart
@@ -1,16 +1,50 @@
 import 'package:intl/intl.dart';
 
 abstract class FormateDate {
-  static String formatDate(String? dateStr) {
-    if (dateStr == null) {
-      return DateFormat.yMMMMd('en_US').format(DateTime.now());
-    } else {
-      try {
-        final date = DateTime.parse(dateStr);
-        return DateFormat.yMMMMd('en_US').format(date);
-      } catch (e) {
-        return DateFormat.yMMMMd('en_US').format(DateTime.now());
-      }
+  /// Example: December 10, 2025
+  static String formatLong(String? dateStr) {
+    return _format(dateStr, DateFormat.yMMMMd('en_US'));
+  }
+
+  /// Example: 10 Dec 2025
+  static String formatMedium(String? dateStr) {
+    return _format(dateStr, DateFormat('d MMM yyyy', 'en_US'));
+  }
+
+  /// Example: 10/12/2025
+  static String formatShort(String? dateStr) {
+    return _format(dateStr, DateFormat('dd/MM/yyyy', 'en_US'));
+  }
+
+  /// Example: 2025-12-10
+  static String formatIso(String? dateStr) {
+    return _format(dateStr, DateFormat('yyyy-MM-dd', 'en_US'));
+  }
+
+  /// Example: 10 Dec 2025 - 14:22
+  static String formatWithTime(String? dateStr) {
+    return _format(dateStr, DateFormat('d MMM yyyy - HH:mm', 'en_US'));
+  }
+
+  /// Wednesday, December 10, 2025
+  static String fullDay(String? dateStr) =>
+      _format(dateStr, DateFormat('EEEE, MMMM d, yyyy'));
+
+  /// Wed, 10 Dec 2025
+  static String fullDayShort(String? dateStr) =>
+      _format(dateStr, DateFormat('EEE, d MMM yyyy'));
+
+  /// 10 December 2025 at 14:22
+  static String fullWithTime(String? dateStr) =>
+      _format(dateStr, DateFormat('d MMMM yyyy at HH:mm'));
+
+  /// Private shared formatter
+  static String _format(String? dateStr, DateFormat formatter) {
+    try {
+      final date = dateStr == null ? DateTime.now() : DateTime.parse(dateStr);
+      return formatter.format(date);
+    } catch (_) {
+      return formatter.format(DateTime.now());
     }
   }
 }
