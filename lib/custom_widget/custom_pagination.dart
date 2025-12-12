f4169f52f0dfde5fe8006541d1b72ee047e5dd09
youssefmdev22 <youssefmdev22@gmail.com>
2025-12-12 13:36:44 +0200

    add custom pagination

--- lib/custom_widget/custom_pagination.dart
+++ lib/custom_widget/custom_pagination.dart
@@ -5,7 +5,8 @@ class CustomPagination extends StatelessWidget {
   final int numberPages;
   final int initialPage;
   final Function(int)? onPageChange;
-  const CustomPagination({super.key, required this.numberPages, required this.initialPage, required this.onPageChange});
+  final NumberPaginatorController? controller;
+  const CustomPagination({super.key, required this.numberPages, required this.initialPage, required this.onPageChange, this.controller});
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +14,7 @@ class CustomPagination extends StatelessWidget {
       numberPages: numberPages,
       initialPage: initialPage,
       onPageChange: onPageChange,
+      controller: controller,
       child: const SizedBox(
         height: 48,
         child: Row(
