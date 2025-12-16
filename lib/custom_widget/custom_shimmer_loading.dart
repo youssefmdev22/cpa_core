5387c04aa888b2d62419f05ab1378982620dbabc
Moaz Osama <wwwmoazosama525@gmail.com>
2025-12-16 14:49:05 +0200

    feat: refactor build_shimmer

--- lib/custom_widget/custom_shimmer_loading.dart
+++ lib/custom_widget/custom_shimmer_loading.dart
@@ -2,25 +2,29 @@ import 'package:cpa_core/cpa_core.dart';
 import 'package:flutter/material.dart';
 
 Widget buildShimmerDataTable({
-  final int? length,
-  final double? height,
-  final double? separatorHeight,
+  int? length,
+  double? height,
+  double? separatorHeight,
 }) {
   return CustomWidgetWrapperItems(
-    child: ListView.separated(
-      shrinkWrap: true,
-      itemBuilder: (context, index) => Shimmer.fromColors(
-        baseColor: Colors.white.withValues(alpha: 0.15),
-        highlightColor: Colors.white.withValues(alpha: 0.35),
-        child: Container(
-          width: double.infinity,
-          height: height ?? 20,
-          decoration: BoxDecoration(color: Colors.grey.shade300),
-        ),
-      ),
-      separatorBuilder: (context, index) =>
-          SizedBox(height: separatorHeight ?? 15),
-      itemCount: length ?? 10,
+    child: buildShimmerList(
+      length: length,
+      height: height,
+      separatorHeight: separatorHeight,
     ),
+  );
+}
+
+Widget buildShimmerListDataTable({
+  int? length,
+  double? height,
+  double? separatorHeight,
+}) {
+  return ListView.separated(
+    shrinkWrap: true,
+    itemBuilder: (context, index) => buildShimmerItem(height: height),
+    separatorBuilder: (context, index) =>
+        SizedBox(height: separatorHeight ?? 15),
+    itemCount: length ?? 10,
   );
 }
