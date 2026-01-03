b9eb1b7f30ae4ece4574a44408487f5c3b8fa0d4
youssefmdev22 <youssefmdev22@gmail.com>
2026-01-03 17:01:55 +0200

    update avatar base url

--- lib/utils/build_avatar_url.dart
+++ lib/utils/build_avatar_url.dart
@@ -1,6 +1,6 @@
 String buildImageUrl({String? fileName, String? customBaseUrl}) {
   final String name = fileName?.isNotEmpty == true ? fileName! : "avatar1.png";
   final String defaultBaseUrl =
-      customBaseUrl ?? "http://localhost:3000/api/v1/uploads/avatar/";
+      customBaseUrl ?? "https://zlunix.com/api/v1/uploads/avatar/";
   return "$defaultBaseUrl$name";
 }
